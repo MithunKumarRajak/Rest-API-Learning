196be7f7ab7fb8ff407d80042d73350ace620bab
MithunKumarRajak <mithunkumarrajak01012005@gmail.com>
2025-10-20 20:30:16 +0530

    changes in serialization and make deserilization form

--- api/views.py
+++ api/views.py
@@ -7,9 +7,9 @@ from rest_framework.decorators import api_view
 # Create your views here.
 
 
-@api_view(['GET'])  # only GET requests are allowed
+@api_view(['GET', 'POST'])  # allow GET and POST requests
 def students(request):
-    # This is manual serialization of QuerySet to list of dictionaries
+    # This is manual method of serialization of QuerySet to list (Dictionary to JSON)
     # """students = Student.objects.all()
     # students_list = list(students.values())
     # return JsonResponse(students_list, safe=False)"""
@@ -18,8 +18,13 @@ def students(request):
         students = Student.objects.all()
         serializer = studentSerializer(students, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':  # Deserialize or Creating a new Student using Serializer that model
+        serializer = studentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 '''
